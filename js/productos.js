ac3521d00b4fe981b4fc7489e3142612358df7c2
Misael <misa132456@gmail.com>
2022-10-31 07:36:39 -0300

    entrega?

---
+++ js/productos.js
@@ -0,0 +1,30 @@
+const productos = [
+    {
+        id: 1,
+        nombre: "BURGER",
+        precio: 50,
+        img: "https://pngimg.com/uploads/burger_sandwich/burger_sandwich_PNG4114.png",
+        descripcion: "Completa con bacon"
+    },
+    {
+        id: 2,
+        nombre: "BURGER PLUS",
+        precio: 100,
+        img: "https://www.pngitem.com/pimgs/m/219-2195800_burger-png-image-hamburguesa-png-png-transparent-png.png",
+        descripcion: "EXTRA  SAUCES"
+    },
+    {
+        id: 3,
+        nombre: "PAPAS",
+        precio: 150,
+        img: "https://png.pngtree.com/png-vector/20201024/ourlarge/pngtree-clean-smooth-vegetable-potatoes-png-image_2371443.jpg",
+        descripcion: "PAPA SIN PELAR 🥵"
+    },
+    {
+        id: 4,
+        nombre: "AGUA",
+        precio: 200,
+        img: "https://www.kindpng.com/picc/m/628-6288390_botella-de-agua-fuente-alta-hd-png-download.png",
+        descripcion: " AGUA DA VIDA"
+    }
+];
--- jsInicio.js
+++ jsInicio.js
@@ -1,25 +1,72 @@
-const botonAumentar = document.getElementById("boton")
+alert("SOLO VAS A TENER QUE PRESIONAR EL BOTON COMPRAR Y DESPUES FIJARTE EN EL CARRITO DE COMPRAS (NO SE PUEDE CERRAR EL CARRITO AUN :,C 😪")
 
-const span = document.getElementById("span")
-const spannum = document.getElementById("spanNum")
-let cantidad = 1
+const ShopContent = document.getElementById("ShopContent")
+const verCarrito = document.getElementById("verCarrito")
+const modalContainer = document.getElementById("modalContainer")
 
+let carrito = [];
 
-botonAumentar.addEventListener("click", (e) => {
-    const modi = formulario.style.display = "block"
-    spannum.textContent = "BRONCE"
-    span.textContent = cantidad++
-    return modi
+productos.forEach((product) => {
+    let content = document.createElement("div")
+    content.className = "card"
+    content.innerHTML = `
+        <p class= "text">${product.descripcion} </p>
+        <img class= "img" src="${product.img}"
+        <h3 class= "h3">${product.nombre}</h3>
+        <p class= "p">${product.precio} $</p>
+
+    `;
+
+    ShopContent.append(content);
+
+    let comprar = document.createElement("button");
+    comprar.innerText = "comprar";
+    comprar.className = "comprar"
+
+    content.append(comprar)
+
+    comprar.addEventListener("click", (e) => {
+        carrito.push({
+            id: product.id,
+            img: product.img,
+            nombre: product.nombre,
+            precio: product.precio
+        })
+        console.log(carrito)
+    })
 })
 
+verCarrito.addEventListener("click", () => {
+    const modalHeader = document.createElement("div");
+    modalHeader.className = "modal-header"
+    modalHeader.innerHTML = `
+        <h1 class="modal-header-titulo"> Carrito </hi>
 
-const botonDisminuir = document.querySelector(".btnDisminuir")
-const formulario = document.querySelector(".form")
+    `
+    modalContainer.append(modalHeader);
 
-botonDisminuir.addEventListener("click", (e) => {
-    const modi = formulario.style.display = "none"
-    cantidad = 1
-    return modi
+    const modalButton = document.createElement("h1")
+    modalButton.innerText = "X"
+    modalButton.className = "modal-button"
+
+    modalHeader.append(modalButton)
+
+    carrito.forEach((product) => {
+        let carritoContent = document.createElement("div")
+        carritoContent.className = "modalContent"
+        carritoContent.innerHTML = `
+            <img class= "img" src="${product.img}">
+            <h3> ${product.nombre} </h3>
+            <p> ${product.precio} $</p>
+        `;
+
+        modalContainer.append(carritoContent)
+    })
+
+    const total = carrito.reduce((acc, el) => acc + el.precio, 0)
+
+    const totalCompra = document.createElement("div")
+    totalCompra.className = "total-compra"
+    totalCompra.innerHTML = `total a pagar: $ ${total}`
+    modalContainer.append(totalCompra) 
 })
-
-// PONER EL .querrySelectorAll no me esta funcionando me tira error, pero me deja hacerlo con getElementById o cualquier otra. La idea es que todo boton que dice compra me indique el "FORMULARIO"(futuro carrito de compra)
