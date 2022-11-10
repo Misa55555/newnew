2dbed650582088f99036651076e2280402f526d9
Misael <misa132456@gmail.com>
2022-11-10 10:45:27 -0300

    tremendoo

--- js/carrito.js
+++ js/carrito.js
@@ -1,40 +1,5 @@
-const ShopContent = document.getElementById("ShopContent")
-const verCarrito = document.getElementById("verCarrito")
-const modalContainer = document.getElementById("modalContainer")
+    const pintarCarrito = () => {
 
-let carrito = [];
-
-productos.forEach((product) => {
-    let content = document.createElement("div")
-    content.className = "card"
-    content.innerHTML = `
-        <p class= "text">${product.descripcion} </p>
-        <img class= "img" src="${product.img}"
-        <h3 class= "h3">${product.nombre}</h3>
-        <p class= "p">${product.precio} $</p>
-
-    `;
-
-    ShopContent.append(content);
-
-    let comprar = document.createElement("button");
-    comprar.innerText = "comprar";
-    comprar.className = "comprar"
-
-    content.append(comprar)
-
-    comprar.addEventListener("click", (e) => {
-        carrito.push({
-            id: product.id,
-            img: product.img,
-            nombre: product.nombre,
-            precio: product.precio
-        })
-        console.log(carrito)
-    })
-})
-
-verCarrito.addEventListener("click", () => {
     modalContainer.innerHTML= ""
     modalContainer.style.display= "flex"
     const modalHeader = document.createElement("div");
@@ -62,15 +27,50 @@ verCarrito.addEventListener("click", () => {
             <img class= "img" src="${product.img}">
             <h3> ${product.nombre} </h3>
             <p> ${product.precio} $</p>
+            <p>cantidad: ${product.cantidad} </p>
+            <p> Total: ${product.cantidad * product.precio} </p>
         `;
 
         modalContainer.append(carritoContent)
+
+        let eliminar = document.createElement("span");
+
+        eliminar.innerText = "❌";
+        eliminar.className = "deleted-product";
+        carritoContent.append(eliminar)
+
+        eliminar.addEventListener("click", eliminarProducto)
     })
 
-    const total = carrito.reduce((acc, el) => acc + el.precio, 0)
+    const total = carrito.reduce((acc, el) => acc + el.precio * el.cantidad, 0)
 
     const totalCompra = document.createElement("div")
     totalCompra.className = "total-compra"
     totalCompra.innerHTML = `total a pagar: $ ${total}`
     modalContainer.append(totalCompra) 
-})
+}
+
+verCarrito.addEventListener("click", pintarCarrito)
+
+const eliminarProducto = () => {
+    const foundId = carrito.find((Element) => Element.id)
+
+    carrito = carrito.filter((carritoId) => {
+        return carritoId !== foundId
+    })
+    carritoContador()
+    guardarLocal()
+    pintarCarrito()
+}
+
+const carritoContador = () => {
+    cantidadCarrito.style.display = "block"
+
+    const carritoLength = carrito.length
+
+    localStorage.setItem("carritoLength", JSON.stringify(carritoLength))
+
+    cantidadCarrito.innerText = JSON.parse(localStorage.getItem("carritoLength"))
+}
+
+carritoContador()
--- js/productos.js
+++ js/productos.js
@@ -1,30 +1,34 @@
 const productos = [
     {
         id: 1,
-        nombre: "BURGER",
-        precio: 50,
-        img: "https://pngimg.com/uploads/burger_sandwich/burger_sandwich_PNG4114.png",
-        descripcion: "Completa con bacon"
+        nombre: "PELOTA",
+        precio: 4000,
+        img: "https://d22fxaf9t8d39k.cloudfront.net/bb1cb4cb89ea8c2778a834da6d656c46f94b2984311ffee659e14d512dbf304136685.png",
+        descripcion: "TALLA 5",
+        cantidad: 1
     },
     {
         id: 2,
-        nombre: "BURGER PLUS",
+        nombre: "GUANTE ARQUERO",
         precio: 100,
-        img: "https://www.pngitem.com/pimgs/m/219-2195800_burger-png-image-hamburguesa-png-png-transparent-png.png",
-        descripcion: "EXTRA  SAUCES"
+        img: "https://static.dafiti.com.ar/p/reusch-0073-243945-1-product.jpg",
+        descripcion: "SOLO EL DERECHO",
+        cantidad: 1
     },
     {
         id: 3,
-        nombre: "PAPAS",
+        nombre: "PANTALON ARQUERO",
         precio: 150,
-        img: "https://png.pngtree.com/png-vector/20201024/ourlarge/pngtree-clean-smooth-vegetable-potatoes-png-image_2371443.jpg",
-        descripcion: "PAPA SIN PELAR 🥵"
+        img: "https://showsport.vtexassets.com/arquivos/ids/750452-800-auto?v=637675151343430000&width=800&height=auto&aspect=true",
+        descripcion: "CALIDAD ANTE TODO",
+        cantidad: 1
     },
     {
         id: 4,
-        nombre: "AGUA",
+        nombre: "BOTINES",
         precio: 200,
-        img: "https://www.kindpng.com/picc/m/628-6288390_botella-de-agua-fuente-alta-hd-png-download.png",
-        descripcion: " AGUA DA VIDA"
+        img: "https://www.pngplay.com/wp-content/uploads/12/Boots-Free-PNG.png",
+        descripcion: "SIN COMENTARIOS 🥵",
+        cantidad: 1
     }
 ];
